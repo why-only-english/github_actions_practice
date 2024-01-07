238797871be55db6d671a6ab006291d8e14d131c
jgmoon <6813821@tukorea.ac.kr>
2024-01-08 00:03:24 +0900

    Set up

---
+++ src/test/java/com/beanstalk/BeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
